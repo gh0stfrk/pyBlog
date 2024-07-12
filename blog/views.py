42a7dce1a1bdbb2d1d237f671099c8cd8777cabd
gh0stfrk <salmansyyd12@gmail.com>
2024-07-13 01:10:05 +0530

    feat: added similar posts suggestion

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count 
 from taggit.models import Tag
 
 from blog.models import Post
@@ -48,7 +49,14 @@ def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
-    return render(request, "blog/post/detail.html", {"post": post, "comments":comments, "form":form})
+    # Similar Posts 
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, "blog/post/detail.html", {"post": post, "comments":comments, "form":form, "similar_posts":similar_posts})
 
 
 def post_share(request: HttpRequest, post_id: int):
