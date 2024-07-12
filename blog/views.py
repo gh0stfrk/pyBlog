c87bfb647ddc24cba9f93ccbf1d6638ade5f5666
gh0stfrk <salmansyyd12@gmail.com>
2024-07-12 19:02:06 +0530

    feat: added pagination & email post sharing

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from django.http import HttpRequest
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 
 from blog.models import Post
+from blog.forms import EmailPostForm
 
 
-def post_list(request):
-    posts = Post.published.all()
+def post_list(request: HttpRequest):
+    post_list = Post.published.all()
 
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, year, month, day, post):
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+
+def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str):
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
@@ -20,3 +40,33 @@ def post_detail(request, year, month, day, post):
     )
 
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request: HttpRequest, post_id: int):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    sent = False
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Send email
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}'s comments: {cd['comments']}"
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
--- pyBlog/settings.py
+++ pyBlog/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
+# Email C0nf
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+EMAIL_USE_TLS = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
