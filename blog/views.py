74682765bdb046f430f1bc84fe425812feaeeb89
gh0stfrk <salmansyyd12@gmail.com>
2024-07-13 15:01:29 +0530

    feat: added ability to search blogs

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.http import HttpRequest
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-from django.db.models import Count 
+from django.db.models import Count
 from taggit.models import Tag
 
 from blog.models import Post
-from blog.forms import EmailPostForm, CommentForm
+from blog.forms import EmailPostForm, CommentForm, SearchForm
 
 
-def post_list(request: HttpRequest, tag_slug = None):
+def post_list(request: HttpRequest, tag_slug=None):
     post_list = Post.published.all()
 
     tag = None
@@ -27,7 +33,7 @@ def post_list(request: HttpRequest, tag_slug = None):
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
         posts = paginator.page(1)
-    return render(request, "blog/post/list.html", {"posts": posts, 'tag': tag})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 class PostListView(ListView):
@@ -49,14 +55,23 @@ def post_detail(request: HttpRequest, year: int, month: int, day: int, post: str
     comments = post.comments.filter(active=True)
     form = CommentForm()
 
-    # Similar Posts 
+    # Similar Posts
     post_tags_ids = post.tags.values_list("id", flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(
-        same_tags=Count('tags')
-    ).order_by('-same_tags', '-publish')[:4]
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
 
-    return render(request, "blog/post/detail.html", {"post": post, "comments":comments, "form":form, "similar_posts":similar_posts})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
+    )
 
 
 def post_share(request: HttpRequest, post_id: int):
@@ -91,11 +106,7 @@ def post_share(request: HttpRequest, post_id: int):
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED
-    )
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
 
     comment = None
     form = CommentForm(request.POST)
@@ -104,9 +115,31 @@ def post_comment(request, post_id):
         comment = form.save(commit=False)
         comment.post = post
         comment.save()
-    
+
     return render(
-            request,
-            "blog/post/comment.html",
-            {"comment": comment, "post":post, "form":form},
-        )
+        request,
+        "blog/post/comment.html",
+        {"comment": comment, "post": post, "form": form},
+    )
+
+
+def post_search(request: HttpRequest):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = (
+                Post.published.annotate(similarity=TrigramSimilarity("title", query))
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
+    )
--- pyBlog/settings.py
+++ pyBlog/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "django.contrib.sitemaps",
+    "django.contrib.postgres",
     "taggit",
     "blog.apps.BlogConfig"
 ]
@@ -88,9 +89,17 @@ WSGI_APPLICATION = "pyBlog.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default":{
+        "ENGINE":'django.db.backends.postgresql',
+        "NAME":config('DB_NAME'),
+        "USER":config('DB_USER'),
+        "PASSWORD":config('DB_PASSWORD'),
+        "HOST":config('DB_HOST'),
+        "PORT":config('DB_PORT')
     }
 }
 
